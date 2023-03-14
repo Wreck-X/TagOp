fc74882cbe86cddbb48f195a3f613974a8addde6
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-03-15 03:42:25 +0530

    Add: Dropdown, Dropzone Features, and Thematical changes

--- lib/textfield.dart
+++ lib/textfield.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
   String entry;
-  InputField(this.entry);
+  String query;
+  InputField(this.query, this.entry);
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 250,
-      child: TextField(
-          decoration: InputDecoration(
-              hintText: entry,
-              filled: true,
-              fillColor: Color.fromARGB(255, 110, 168, 255))),
+    return Row(
+      children: [
+        Container(
+            width: 100,
+            child: Text(query,
+                style: TextStyle(color: Colors.white, fontSize: 18))),
+        SizedBox(
+          width: 250,
+          height: 50,
+          child: TextField(
+              decoration: InputDecoration(
+                  hintText: entry,
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 110, 168, 255))),
+        ),
+      ],
     );
   }
 }
