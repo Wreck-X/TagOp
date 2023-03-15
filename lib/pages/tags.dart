f7f078efa0c529fca1d9443f41defc73b93a2bf5
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-03-15 17:21:00 +0530

    minor changes

--- lib/pages/tags.dart
+++ lib/pages/tags.dart
@@ -11,12 +11,22 @@ class _TagsPageState extends State<TagsPage> {
   var selectedindex = 0;
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedindex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = Placeholder();
+    }
     return Container(
         child: NavigationRail(
       extended: false,
       destinations: [
         NavigationRailDestination(
-            icon: Icon(Icons.disc_full_rounded), label: Text("something"))
+            icon: Icon(Icons.disc_full_rounded), label: Text("something")),
+        NavigationRailDestination(
+            icon: Icon(Icons.disc_full_rounded), label: Text("somethingelse"))
       ],
       selectedIndex: selectedindex,
       onDestinationSelected: (value) => {
